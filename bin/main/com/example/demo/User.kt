90d9739654cab477750ca3b765883edf9982bf18
Vorrarit Luengwattanakij <vorrarit@pakgon.com>
2018-05-15 03:13:15 +0700

    remove bin folder

--- bin/main/com/example/demo/User.kt
+++
@@ -1,9 +0,0 @@
-package com.example.demo
-
-import java.util.Date
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.Id
-
-@Entity
-data class User(@Id @GeneratedValue val id:Int=0, val name:String="", val birthDate:Date?=null)
--- bin/main/data.sql
+++
@@ -1,3 +0,0 @@
-insert into user values(1, sysdate(), 'Bob');
-insert into user values(2, sysdate(), 'May');
-insert into user values(3, sysdate(), 'Dan');
